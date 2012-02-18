334b31b92b30f6eca6c070a0b61e16a21e669478
Strzelewicz Alexandre <strzelewicz.alexandre@gmail.com>
2012-02-18 12:15:07 +0100

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TAHProject::Application.config.secret_token = '686b8567a5f23dc6199435d46fcc192756dd9027b843b6696ac54aa037b9293418dd46e901fd39a038f125564a5d5e471ff28680e6315431625c050446bd592e'
