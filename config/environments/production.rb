f2e8a4993e655336691e2760c2537b5d8624b827
Strzelewicz Alexandre <strzelewicz.alexandre@gmail.com>
2012-03-02 22:55:17 +0100

    ok

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,12 +15,17 @@ TAHProject::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
 
-  config.assets.precompile = ['*.js']
+  config.i18n.fallbacks = true
+
+  # Send deprecation notices to registered listeners
+  config.active_support.deprecation = :notify
+
+  #config.assets.precompile = ['*.js']
 
 
   
@@ -61,10 +66,6 @@ TAHProject::Application.configure do
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found)
-  config.i18n.fallbacks = true
-
-  # Send deprecation notices to registered listeners
-  config.active_support.deprecation = :notify
 
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
