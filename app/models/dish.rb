1bed46423f19d83ba5713856b1417b516d2c9882
Sebastien Guignot <kxu@tsys.org>
2012-03-26 23:46:16 +0200

    annotate update

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -2,18 +2,22 @@
 #
 # Table name: dishes
 #
-#  id                 :integer         not null, primary key
-#  title              :string(255)
-#  description        :text
-#  menu_id            :integer
-#  created_at         :datetime        not null
-#  updated_at         :datetime        not null
-#  position           :integer
-#  category           :integer
-#  image_file_name    :string(255)
-#  image_content_type :string(255)
-#  image_file_size    :integer
-#  image_updated_at   :datetime
+#  id                      :integer         not null, primary key
+#  title                   :string(255)
+#  description             :text
+#  menu_id                 :integer
+#  created_at              :datetime        not null
+#  updated_at              :datetime        not null
+#  position                :integer
+#  main_image_file_name    :string(255)
+#  main_image_content_type :string(255)
+#  main_image_file_size    :integer
+#  main_image_updated_at   :datetime
+#  category                :integer
+#  image_file_name         :string(255)
+#  image_content_type      :string(255)
+#  image_file_size         :integer
+#  image_updated_at        :datetime
 #
 
 class Dish < ActiveRecord::Base
